85fcfe8edd162e94a2e1ba20a4cc008451a522b1
yashar <115088770+yasharzavary@users.noreply.github.com>
2026-09-21 15:28:13 +0330

    category model added to admin panel

--- topic_learning_management/apps/project_app/admin.py
+++ topic_learning_management/apps/project_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
+    ordering = ("name",)
+
+    fieldsets = (
+        (
+            "Category Information",
+            {
+                "fields": ("name", "description"),
+            },
+        ),
+    )
