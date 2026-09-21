a3ac988268ca39dc468eddecbf5c4c0ffbcd9ad5
yashar <115088770+yasharzavary@users.noreply.github.com>
2026-09-21 16:08:45 +0330

    topic detail functions created

--- topic_learning_management/apps/project_app/urls.py
+++ topic_learning_management/apps/project_app/urls.py
@@ -4,5 +4,55 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('create/', create_topic, name='create_topic')
+    path('create/', create_topic, name='create_topic'),
+
+    # Topic detail page
+    path(
+        "<uuid:topic_id>/",
+        topic_detail,
+        name="topic_detail"
+    ),
+
+    # Lifecycle actions
+    path(
+        "<uuid:topic_id>/start-research/",
+        start_research,
+        name="start_research"
+    ),
+    path(
+        "<uuid:topic_id>/finish-research/",
+        finish_research,
+        name="finish_research"
+    ),
+    path(
+        "<uuid:topic_id>/start-learning/",
+        start_learning,
+        name="start_learning"
+    ),
+    path(
+        "<uuid:topic_id>/finish-learning/",
+        finish_learning,
+        name="finish_learning"
+    ),
+    path(
+        "<uuid:topic_id>/start-experience/",
+        start_experience,
+        name="start_experience"
+    ),
+    path(
+        "<uuid:topic_id>/finish-experience/",
+        finish_experience,
+        name="finish_experience"
+    ),
+    path(
+        "<uuid:topic_id>/complete/",
+        complete_topic,
+        name="complete_topic"
+    ),
+    # Delete
+    path(
+        "<uuid:topic_id>/delete/",
+        delete_topic,
+        name="delete_topic"
+    ),
 ]
--- topic_learning_management/apps/project_app/views.py
+++ topic_learning_management/apps/project_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
 
 def custom_404(request, exception):
 
@@ -21,30 +24,62 @@ def index(request):
 
 
 
-
-
 def create_topic(request):
     categories = Category.objects.all()
+
     if request.method == "POST":
-        title = request.POST.get("title", "").strip()
-        description = request.POST.get("description", "").strip()
-        category_id = request.POST.get("category")
-        priority = request.POST.get("priority", "MEDIUM")
-        learning_goal = request.POST.get("learning_goal", "").strip()
-        origin_reason = request.POST.get("origin_reason", "").strip()
-        origin_file = request.FILES.get("origin_file")
+        title = request.POST.get(
+            "title",
+            ""
+        ).strip()
+
+        description = request.POST.get(
+            "description",
+            ""
+        ).strip()
+
+        category_id = request.POST.get(
+            "category"
+        )
+
+        priority = request.POST.get(
+            "priority",
+            "MEDIUM"
+        )
+
+        learning_goal = request.POST.get(
+            "learning_goal",
+            ""
+        ).strip()
+
+        origin_reason = request.POST.get(
+            "origin_reason",
+            ""
+        ).strip()
+
+        origin_file = request.FILES.get(
+            "origin_file"
+        )
 
         if not title:
-            
-            messages.error(request, "Topic title is required.")
+            messages.error(
+                request,
+                "Topic title is required."
+            )
             return render(
                 request,
-                "create.html",
-                {"categories": categories},
+                "topics/create.html",
+                {
+                    "categories": categories
+                },
             )
 
-        category = get_object_or_404(Category, id=category_id)
+        category = get_object_or_404(
+            Category,
+            id=category_id
+        )
 
+        # Create Topic
         topic = Topic.objects.create(
             title=title,
             description=description,
@@ -53,14 +88,226 @@ def create_topic(request):
             learning_goal=learning_goal,
             origin_reason=origin_reason,
             origin_file=origin_file,
+            status="IDEA"
         )
 
-        messages.success(request, "Topic created successfully.")
+        # Create Topic Timeline
+        TopicTimeline.objects.create(
+            topic=topic,
+            topic_created=timezone.now()
+        )
+        messages.success(
+            request,
+            "Topic created successfully."
+        )
 
-        return redirect("index")
+        return redirect(
+            "index"
+        )
 
     context = {
         "categories": categories,
     }
 
-    return render(request, "topics/create.html", context)
+    return render(
+        request,
+        "topics/create.html",
+        context
+    )
+
+@login_required
+def topic_detail(request, topic_id):
+
+
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = get_object_or_404(
+        TopicTimeline,
+        topic=topic
+    )
+
+    context = {
+        "topic": topic,
+        "timeline": timeline
+    }
+
+    return render(
+        request,
+        "topics/detail.html",
+        context
+    )
+
+
+@login_required
+def start_research(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+
+    timeline = topic.timeline
+
+    if topic.status == "IDEA":
+        topic.status = "SOURCE_RESEARCH"
+        topic.save()
+        timeline.source_research_start = (
+            timezone.now()
+        )
+
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def finish_research(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = topic.timeline
+    if topic.status == "SOURCE_RESEARCH":
+        timeline.source_research_finish = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def start_learning(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = topic.timeline
+
+    if (
+        topic.status == "SOURCE_RESEARCH"
+        and
+        timeline.source_research_finish
+    ):
+        topic.status = "LEARNING"
+        topic.save()
+        timeline.learning_start = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def finish_learning(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = topic.timeline
+
+    if topic.status == "LEARNING":
+        timeline.learning_finish = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def start_experience(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = topic.timeline
+
+    if (
+        topic.status == "LEARNING"
+        and
+        timeline.learning_finish
+    ):
+        topic.status = "EXPERIENCE"
+        topic.save()
+
+        timeline.experience_start = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def finish_experience(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+
+    timeline = topic.timeline
+    if topic.status == "EXPERIENCE":
+
+        timeline.experience_finish = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+
+@login_required
+def complete_topic(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+    timeline = topic.timeline
+
+    if (
+        topic.status == "EXPERIENCE"
+        and
+        timeline.experience_finish
+    ):
+
+        topic.status = "COMPLETED"
+        topic.save()
+        timeline.topic_finish = (
+            timezone.now()
+        )
+        timeline.save()
+
+    return redirect(
+        "topic_detail",
+        topic_id=topic.id
+    )
+
+@login_required
+def delete_topic(request, topic_id):
+    topic = get_object_or_404(
+        Topic,
+        id=topic_id
+    )
+
+    if request.method == "POST":
+        topic.delete()
+
+    return redirect(
+        "index"
+    )
